cba5c741f34280c1fd790ea4663a0e49175ebf3e
Mihai Timoficiuc <mihaitimoficiuc@gmail.com>
2023-10-27 23:26:36 +0200

    team card hover dialog

--- src/components/ui/TeamCard/index.tsx
+++ src/components/ui/TeamCard/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+import useIsMobile from '@/hooks/useIsMobile';
 import dotsIcon from '../../../assets/icons/green/dots.png';
+import TeamDialog from '../TeamDialog';
 interface TeamCardProps {
   src: string;
   title: string;
@@ -12,46 +15,67 @@ const TeamCard: React.FC<TeamCardProps> = ({
   text,
   reverse,
 }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const isMobile = useIsMobile();
+
+  const handleOpenModal = () => {
+    if (isMobile) {
+      setModalOpen(true);
+    }
+  };
+
   return (
-    <div
-      className={`lg:flex ${
-        reverse ? 'flex-row-reverse' : 'flex-row'
-      } gap-28 justify-center items-center group cursor-pointer rounded-sm lg:border-2 lg:border-transparent lg:hover:bg-primary lg:hover:border-accent transition-all duration-300`}
-    >
+    <>
       <div
-        className="w-[264px] h-[183px] lg:min-w-[472px] lg:h-[373px] p-6 rounded-sm lg:rounded-none border-2 lg:border-none border-secondary relative overflow-hidden bg-cover bg-no-repeat bg-center"
-        style={{
-          backgroundImage: `url(${src})`,
-        }}
+        className={`lg:flex ${
+          reverse ? 'flex-row-reverse' : 'flex-row'
+        } gap-28 justify-center items-center group cursor-pointer rounded-sm lg:border-2 lg:border-transparent lg:hover:bg-primary lg:hover:border-accent transition-all duration-300`}
+        onClick={handleOpenModal}
       >
         <div
-          className="absolute inset-0 p-4"
+          className="w-[264px] h-[183px] lg:min-w-[472px] lg:h-[373px] p-6 rounded-sm lg:rounded-none border-2 lg:border-none border-secondary relative overflow-hidden bg-cover bg-no-repeat bg-center"
           style={{
-            background:
-              'linear-gradient(0deg, rgba(13, 10, 11, 0.65) 0%, rgba(13, 10, 11, 0.00) 100%)',
+            backgroundImage: `url(${src})`,
           }}
         >
-          <div className="h-full flex items-end justify-between lg:hidden">
-            <h2 className="text-2xl font-light text-primary-foreground leading-none">
-              {title}
-            </h2>
-            <img
-              className="w-7 h-7"
-              src={dotsIcon}
-              alt="Icon"
-            />
+          <div
+            className="absolute inset-0 p-4"
+            style={{
+              background:
+                'linear-gradient(0deg, rgba(13, 10, 11, 0.65) 0%, rgba(13, 10, 11, 0.00) 100%)',
+            }}
+          >
+            <div className="h-full flex items-end justify-between lg:hidden">
+              <h2 className="text-2xl font-light text-primary-foreground leading-none">
+                {title}
+              </h2>
+              <img
+                className="w-7 h-7"
+                src={dotsIcon}
+                alt="Icon"
+              />
+            </div>
           </div>
         </div>
+        <div className="hidden lg:flex flex-col gap-4 lg:group-hover:p-12">
+          <h2 className="text-2xl font-light leading-none lg:group-hover:text-accent lg:group-hover:underline">
+            {title}
+          </h2>
+          <p className="text-xl font-extralight lg:group-hover:text-primary-foreground">
+            {text}
+          </p>
+        </div>
       </div>
-      <div className="hidden lg:flex flex-col gap-4 lg:group-hover:p-12">
-        <h2 className="text-2xl font-light leading-none lg:group-hover:text-accent lg:group-hover:underline">
-          {title}
-        </h2>
-        <p className="text-xl font-extralight lg:group-hover:text-primary-foreground">
-          {text}
-        </p>
-      </div>
-    </div>
+      {isMobile && (
+        <TeamDialog
+          open={modalOpen}
+          setModalOpen={setModalOpen}
+          srcBg={src}
+          title={title}
+          text={text}
+        />
+      )}
+    </>
   );
 };
 
---
+++ src/components/ui/TeamDialog/index.tsx
@@ -0,0 +1,59 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from '../Dialog';
+
+interface TeamDialogProps {
+  open: boolean;
+  setModalOpen: (open: boolean) => void;
+  srcBg: string;
+  title: string;
+  text: string;
+}
+
+const TeamDialog: React.FC<TeamDialogProps> = ({
+  open,
+  setModalOpen,
+  srcBg,
+  title,
+  text,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setModalOpen}
+      modal={true}
+    >
+      <DialogContent className="max-w-[80%] p-0 gap-0 rounded-xl outline-offset-0 border-4 border-accent">
+        <div
+          className="w-full h-[183px] relative bg-cover bg-no-repeat bg-center rounded-t-lg"
+          style={{
+            backgroundImage: `url(${srcBg})`,
+          }}
+        >
+          <div
+            className="absolute inset-0 z-0 rounded-t-lg"
+            style={{
+              background:
+                'linear-gradient(0deg, rgba(13, 10, 11, 0.65) 0%, rgba(13, 10, 11, 0.00) 100%)',
+            }}
+          ></div>
+
+          <div className="flex flex-col h-full gap-2 px-4 justify-end p-1">
+            <DialogTitle className="text-primary-foreground text-2xl font-light z-10">
+              {title}
+            </DialogTitle>
+          </div>
+        </div>
+
+        <DialogFooter className="text-primary-foreground text-base font-extralight rounded-b-lg px-6 py-10 bg-primary">
+          <p>{text}</p>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TeamDialog;
