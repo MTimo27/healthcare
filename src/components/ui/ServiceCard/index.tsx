1ec68949ddd6670dd01c725075df922bb8c8b9ca
Mihai Timoficiuc <mihaitimoficiuc@gmail.com>
2023-10-27 23:19:37 +0200

    hover dialog services

--- src/components/ui/ServiceCard/index.tsx
+++ src/components/ui/ServiceCard/index.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import ServiceDialog from '../ServiceDialog';
+import useIsMobile from '@/hooks/useIsMobile';
+
 interface ServiceCardProps {
   srcBg: string;
   iconSrc: string;
@@ -13,49 +17,71 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   title,
   text,
 }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const isMobile = useIsMobile();
+
+  const handleOpenModal = () => {
+    if (isMobile) {
+      setModalOpen(true);
+    }
+  };
+
   return (
-    <div
-      className="w-full h-[154px] lg:min-w-[373px] lg:h-[200px] rounded-sm border-2 border-secondary relative overflow-hidden bg-cover bg-no-repeat bg-center group cursor-pointer lg:p-6"
-      style={{
-        backgroundImage: `url(${srcBg})`,
-      }}
-    >
+    <>
       <div
-        className="absolute inset-0"
+        className="w-full h-[154px] lg:min-w-[373px] lg:h-[200px] rounded-sm border-2 border-secondary relative overflow-hidden bg-cover bg-no-repeat bg-center group cursor-pointer lg:p-6"
         style={{
-          background:
-            'linear-gradient(90deg, #0D0A0B 0%, rgba(13, 10, 11, 0.27) 127.55%)',
+          backgroundImage: `url(${srcBg})`,
         }}
-      ></div>
+        onClick={handleOpenModal}
+      >
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              'linear-gradient(90deg, #0D0A0B 0%, rgba(13, 10, 11, 0.27) 127.55%)',
+          }}
+        ></div>
 
-      <div className="absolute inset-0 bg-black transition-opacity duration-100 opacity-0 z-10 lg:group-hover:opacity-100"></div>
+        <div className="absolute inset-0 bg-black transition-opacity duration-100 opacity-0 z-10 lg:group-hover:opacity-100"></div>
 
-      <div className="h-full flex items-center justify-center">
-        <div className="flex flex-col items-center lg:items-start justify-center z-20 gap-4 lg:gap-0">
-          <div className="icon-container relative w-12 h-11 lg:mb-6">
-            <img
-              className="w-full h-full icon absolute transition-opacity duration-300 opacity-100 lg:group-hover:opacity-0"
-              src={iconSrc}
-              alt="Icon"
-            />
-            {iconHoverSrc && (
+        <div className="h-full flex items-center justify-center">
+          <div className="flex flex-col items-center lg:items-start justify-center z-20 gap-4 lg:gap-0">
+            <div className="icon-container relative w-12 h-11 lg:mb-6">
               <img
-                className="w-full h-full filled-icon absolute transition-opacity duration-300 opacity-0 lg:group-hover:opacity-100"
-                src={iconHoverSrc}
+                className="w-full h-full icon absolute transition-opacity duration-300 opacity-100 lg:group-hover:opacity-0"
+                src={iconSrc}
                 alt="Icon"
               />
-            )}
-          </div>
+              {iconHoverSrc && (
+                <img
+                  className="w-full h-full filled-icon absolute transition-opacity duration-300 opacity-0 lg:group-hover:opacity-100"
+                  src={iconHoverSrc}
+                  alt="Icon"
+                />
+              )}
+            </div>
 
-          <h2 className="text-3xl font-light lg:mb-2 text-primary-foreground lg:group-hover:text-secondary lg:group-hover:underline leading-none">
-            {title}
-          </h2>
-          <p className="hidden lg:block text-base text-primary-foreground font-extralight leading-none">
-            {text}
-          </p>
+            <h2 className="text-3xl font-light lg:mb-2 text-primary-foreground lg:group-hover:text-secondary lg:group-hover:underline leading-none">
+              {title}
+            </h2>
+            <p className="hidden lg:block text-base text-primary-foreground font-extralight leading-none">
+              {text}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      {isMobile && (
+        <ServiceDialog
+          open={modalOpen}
+          setModalOpen={setModalOpen}
+          srcBg={srcBg}
+          iconSrc={iconSrc}
+          title={title}
+          text={text}
+        />
+      )}
+    </>
   );
 };
 
---
+++ src/components/ui/ServiceDialog/index.tsx
@@ -0,0 +1,65 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from '../Dialog';
+
+interface ServiceDialogProps {
+  open: boolean;
+  setModalOpen: (open: boolean) => void;
+  srcBg: string;
+  iconSrc: string;
+  title: string;
+  text: string;
+}
+
+const ServiceDialog: React.FC<ServiceDialogProps> = ({
+  open,
+  setModalOpen,
+  srcBg,
+  iconSrc,
+  title,
+  text,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setModalOpen}
+      modal={true}
+    >
+      <DialogContent className="max-w-[80%] p-0 gap-0 rounded-xl outline-offset-0 border-4 border-accent">
+        <div
+          className="w-full h-[183px] relative bg-cover bg-no-repeat bg-center rounded-t-lg"
+          style={{
+            backgroundImage: `url(${srcBg})`,
+          }}
+        >
+          <div
+            className="absolute inset-0 z-0 rounded-t-lg"
+            style={{
+              background: 'rgba(13, 10, 11, 0.65)',
+            }}
+          ></div>
+
+          <div className="flex flex-col h-full gap-2 px-4 justify-end p-1">
+            <img
+              className="w-12 h-11 z-10"
+              src={iconSrc}
+              alt="Icon"
+            />
+            <DialogTitle className="text-primary-foreground text-2xl font-light z-10">
+              {title}
+            </DialogTitle>
+          </div>
+        </div>
+
+        <DialogFooter className="text-primary-foreground text-base font-extralight rounded-b-lg px-6 py-10 bg-primary">
+          <p>{text}</p>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ServiceDialog;
